3526cb50b898642e6fd02a2b47f70d80c6ed7e69
Mohit <53579940+Junglee21@users.noreply.github.com>
2024-03-10 21:24:54 +0530

    added gemini api

--- src/main/java/com/mo/sololeveling/SoloLevelingApplication.java
+++ src/main/java/com/mo/sololeveling/SoloLevelingApplication.java
@@ -2,8 +2,9 @@ package com.mo.sololeveling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SoloLevelingApplication {
 
 	public static void main(String[] args) {
